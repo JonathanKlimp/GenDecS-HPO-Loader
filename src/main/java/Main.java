c23f8d18289342441b78e3932028dc5fb9acd850
JonathanKlimp <78917640+JonathanKlimp@users.noreply.github.com>
2022-03-02 16:57:22 +0100

    moved to hp.owl file in a data dir. And read the loaded the file with Apache Jena

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,36 @@
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.*;
+import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+import org.apache.jena.util.iterator.ExtendedIterator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
-        OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM, null);
-        m.read("file:hp.owl");
+        OntModel model = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM);
 
-        System.out.println(m.getSubGraphs().size());
+        String filename = "data/hp.owl";
+        try {
+            InputStream inputStream = new FileInputStream(filename);
+            model.read(inputStream, "OWL/XML");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+//        System.out.println(model.getSubGraphs().size());
+
+        ExtendedIterator<OntClass> itI = model.listClasses();
+
+
+        while (itI.hasNext()) {
+            OntClass i = itI.next();
+//            System.out.println(i);
+//            System.out.println(i.getLabel(null));
+//            System.out.println(i.getLocalName());
+        }
+        System.out.println("End of main");
     }
 }
